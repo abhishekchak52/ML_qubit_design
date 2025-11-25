2529ad0fafde4b5494ac999300bc9209d19ad66f
OliviaSeidel <seidelolivia2@gmail.com>
2025-11-25 19:17:28 +0000

    tuned hyperparameters for coupler capacitance matrix

--- model_predict_coupler_NCap_cap_matrix/parameters.py
+++ model_predict_coupler_NCap_cap_matrix/parameters.py
@@ -2,8 +2,8 @@
 DATA_DIR = 'data'
 DATASETS_JSON = DATA_DIR + '/datasets.json'
 
-KERAS_TUNER = True
-KERAS_TUNER_TRIALS = 50
+KERAS_TUNER = False
+KERAS_TUNER_TRIALS = 170
 KERAS_DIR = 'keras'
 
 ENCODING_TYPE = 'one hot' # need to pass 'one hot' or 'linear' or 'Try Both'
@@ -14,14 +14,14 @@ DATA_AUGMENTATION = True
 # We use a simple fully connected network (MLP) 
 # 4 layers because deeper NNs can capture more complex patterns
 # Gradually decrease the neuron size to better capture patterns while avoiding overfitting
-NEURONS_PER_LAYER = [1600,1300,700,600]
+NEURONS_PER_LAYER = [1000,3800,300,400]
 TRAIN_DROPOUT_RATE = 0.4
 
 # Training hyper-parameters
 
 # Learning Rate gives the step size that the optimizer takes while learning, 
 # smaller step size means slower convergence but more accuracy
-LR_INITIAL = 0.001
+LR_INITIAL = 0.000982
 
 # Learning rate decay helps the model become refined as it gets closer to a minimum
 # The learning rate decay steps desides how many steps the learning rate will decay after
