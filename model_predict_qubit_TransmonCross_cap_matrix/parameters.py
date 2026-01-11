06e3b949253118c2d1571bcdd8487c0d5e0a02ef
OliviaSeidel <seidelolivia2@gmail.com>
2026-01-11 17:31:00 +0000

    replaced data loading script for qubit transmon cap matrix to be the cap values rather than the H params again

--- model_predict_qubit_TransmonCross_cap_matrix/parameters.py
+++ model_predict_qubit_TransmonCross_cap_matrix/parameters.py
@@ -1,9 +1,15 @@
+# transmon cross cap matrix
+
 # Where the datasets are
 DATA_DIR = 'data'
 DATASETS_JSON = DATA_DIR + '/datasets.json'
 
+SWEEP_PARAM_NUM = False
+SWEEP_DATA_AMOUNT = False
+VISUALIZE_GRADIENTS = False
+
 KERAS_TUNER = True
-KERAS_TUNER_TRIALS = 200
+KERAS_TUNER_TRIALS = 1658
 KERAS_DIR = 'keras'
 
 ENCODING_TYPE = 'one hot' # need to pass 'one hot' or 'linear' or 'Try Both'
@@ -14,18 +20,19 @@ DATA_AUGMENTATION = True
 # We use a simple fully connected network (MLP) 
 # 4 layers because deeper NNs can capture more complex patterns
 # Gradually decrease the neuron size to better capture patterns while avoiding overfitting
-NEURONS_PER_LAYER = [2900, 1400, 1200, 1800]
-TRAIN_DROPOUT_RATE = 0 #lower bound for tuning dropout rate in keras
+NEURONS_PER_LAYER = [1024]
+TRAIN_DROPOUT_RATE = 0.05
 
 # Training hyper-parameters
 
 # Learning Rate gives the step size that the optimizer takes while learning, 
 # smaller step size means slower convergence but more accuracy
-LR_INITIAL = 0.0014041690155863762
+# learning rate is=LR_INITIAL×(LR_DECAY_RATE)^(t/LR_DECAY_STEPS)
+LR_INITIAL = 0.000982
 
 # Learning rate decay helps the model become refined as it gets closer to a minimum
 # The learning rate decay steps desides how many steps the learning rate will decay after
-LR_DECAY_STEPS = 100  # 100 best for log phig1 cadence data
+LR_DECAY_STEPS = 35  # 100 best for log phig1 cadence data
 
 # LR_INITIAL * LR_DECAY_RATE after each number of LR_DECAY_STEPS
 LR_DECAY_RATE = 0.99
@@ -33,12 +40,10 @@ LR_DECAY_RATE = 0.99
 # Staircase or continuous?
 LR_STAIRCASE = False
 
-TRAIN_EARLY_STOPPING_PATIENCE = 50
-TRAIN_BATCH_SIZE = 32 # 32 default
+TRAIN_EARLY_STOPPING_PATIENCE = 400
+TRAIN_BATCH_SIZE = 128 # 32 default
 #TRAIN_VALIDATION_SPLIT = 0.2
 
-TRAIN_LOSS = 'mean_squared_error'
+#TRAIN_LOSS = 'mean_squared_error'
+TRAIN_LOSS = 'mae' # mean absolute error
 #TRAIN_LOSS = 'mean_squared_logarithmic_error'
-
-
-
