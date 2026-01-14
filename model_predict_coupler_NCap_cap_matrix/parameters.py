fc2a8799d53292502eaf62798fd2c6d1a9b50353
OliviaSeidel <seidelolivia2@gmail.com>
2026-01-14 23:09:01 +0000

    coupler best model

--- model_predict_coupler_NCap_cap_matrix/parameters.py
+++ model_predict_coupler_NCap_cap_matrix/parameters.py
@@ -8,7 +8,7 @@ SWEEP_PARAM_NUM = False
 SWEEP_DATA_AMOUNT = False
 VISUALIZE_GRADIENTS = False
 
-KERAS_TUNER = True
+KERAS_TUNER = False
 KERAS_TUNER_TRIALS = 2000
 KERAS_DIR = 'keras'
 
@@ -20,15 +20,15 @@ DATA_AUGMENTATION = True
 # We use a simple fully connected network (MLP) 
 # 4 layers because deeper NNs can capture more complex patterns
 # Gradually decrease the neuron size to better capture patterns while avoiding overfitting
-NEURONS_PER_LAYER = [1024]
-TRAIN_DROPOUT_RATE = 0 #0.05
+NEURONS_PER_LAYER = [264,964,764,664]#[1024]
+TRAIN_DROPOUT_RATE = 0
 
 # Training hyper-parameters
 
 # Learning Rate gives the step size that the optimizer takes while learning, 
 # smaller step size means slower convergence but more accuracy
 # learning rate is=LR_INITIAL×(LR_DECAY_RATE)^(t/LR_DECAY_STEPS)
-LR_INITIAL = 0.000982
+LR_INITIAL = 0.0007026341305552234
 
 # Learning rate decay helps the model become refined as it gets closer to a minimum
 # The learning rate decay steps desides how many steps the learning rate will decay after
